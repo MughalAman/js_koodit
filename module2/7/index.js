1aa562015740219f1ce38fae14f20c8ae07a36ab
Amppazz <amppa.mughal@gmail.com>
2022-11-11 15:11:50 +0200

    done

---
+++ module2/7/index.js
@@ -0,0 +1,15 @@
+function RollDice(sides) {
+    return Math.floor(Math.random() * sides) + 1;
+}
+
+let last_roll = 0;
+const ul = document.getElementById('list');
+const sides = Number(prompt('How many sides should the dice have?'));
+
+while (last_roll !== sides){
+    const roll = RollDice();
+    last_roll = roll;
+    const li = document.createElement('li');
+    li.innerText = roll;
+    ul.appendChild(li);
+}
